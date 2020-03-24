b37b105b85bfe042fa35a1658a7ba2aa0d3fd8ce
carlosrrod <carlosrrdev@gmail.com>
2020-03-24 10:12:23 -0500

    Add Basic Javascript

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-// Attach buttons to DOM
-const addProjectCardBtn = document.querySelector('.add-card-btn');
-const addProjectCardBodyBtn = document.querySelector('.add-card-body');
+// Connect to DOM Elements
+const cardsContainer = document.querySelector('.cards-container');
+const placeholderCard = document.getElementById('placeholder-card');
 
- // Listen for clicks on buttons
- addProjectCardBtn.addEventListener('click', e => {
-   console.log('add project card button was clicked');
- })
+// User clicks placeholderCard
+placeholderCard.addEventListener('click', e => {
+  placeholderCard.classList.toggle('hide-placeholder');
+  createNewCard();
+})
 
- addProjectCardBodyBtn.addEventListener('click', e => {
-   console.log('add card body button was clicked');
- })
+// Create New Card
+function createNewCard(){
+  const projectCardContainer = document.createElement('section');
+  projectCardContainer.classList.add('project-card-container');
+
+  const projectCardTitle = document.createElement('section');
+  projectCardTitle.classList.add('project-card-title');
+  projectCardContainer.appendChild(projectCardTitle);
+
+  const projectCardTitleInput = document.createElement('input');
+  projectCardTitleInput.type = 'text';
+  projectCardTitleInput.placeholder = 'New Project Card';
+  projectCardTitle.appendChild(projectCardTitleInput);
+
+  const projectCardDeleteBtn = document.createElement('div');
+  projectCardDeleteBtn.classList.add('project-card-delete-btn');
+  projectCardDeleteBtn.innerText = 'X';
+  projectCardTitle.appendChild(projectCardDeleteBtn);
+
+  const projectCardBody = document.createElement('div');
+  projectCardBody.contentEditable = 'true';
+  projectCardBody.classList.add('project-card-body');
+  projectCardContainer.appendChild(projectCardBody);
+  
+  // User Clicks Delete Card Button
+  projectCardDeleteBtn.addEventListener('click', () => {
+    cardsContainer.removeChild(projectCardContainer);
+    placeholderCard.classList.toggle('hide-placeholder');
+  });
+  projectCardContainer.classList.add('card-animate');
+  cardsContainer.appendChild(projectCardContainer);
+}
